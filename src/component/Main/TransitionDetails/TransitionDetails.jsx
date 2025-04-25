8483fb568988029118810460c6a994e4a52118f1
Aminulhoque01 <aminuhoque53@gmail.com>
2025-04-25 16:47:07 +0600

    new commit

--- src/component/Main/TransitionDetails/TransitionDetails.jsx
+++ src/component/Main/TransitionDetails/TransitionDetails.jsx
@@ -3,49 +3,86 @@ import user from '../../../assets/provider/provider.jpg'
 
 
 
-const TransitionDetails=()=>{
-    return(
+const TransitionDetails = () => {
+    return (
         <div className="w-full container mx-auto px-5">
             <h1 className='text-2xl mb-5'>Transcation Details</h1>
             <div className="flex border border-[#1EB9C6] justify-between p-10 ">
 
                 <div className="flex">
-                    <div>
-                        <img src={user} className='w-20 h-20 rounded-full'></img>
+                    <div className=''>
+
+                        <img src={user} className='w-40 h-40 rounded-full'></img>
                     </div>
-                    <div className='pt-2 ml-2'>
-                        <p className='text-xl'>User Name</p>
-                        <h2 className='text-2xl'>mr.provider</h2>
+                    <div className='ml-2'>
+                        <span className='text-2xl mb-5 font-bold'>From:</span>
+                        <p className='text-md'>User Name</p>
+                        <h2 className='text-2xl font-bold'>mr.provider</h2>
+                        <br />
+                        <p>Email</p>
+                        <p className='text-black'>ami@gmail.com</p>
                     </div>
                 </div>
                 <div>
                     <p>Amount</p>
-                    <h2>150 CHF</h2>
+                    <h2 className='text-xl text-black font-bold'>150 CHF</h2>
                     <br />
                     <p>Transcation ID</p>
-                    <h2>121511052585</h2>
+                    <h2 className='text-xl text-black font-bold'>121511052585</h2>
                 </div>
                 <div>
-                    <p>Role</p>
-                    <h1>Coustomer</h1>
-                        <br />
+                    <p >Role</p>
+                    <h1 className='text-xl text-black '>Coustomer</h1>
+                    <br />
                     <p>Reason</p>
-                    <h1>Subscription Purchase</h1>
+                    <h1 className='text-xl text-black '>Subscription Purchase</h1>
                 </div>
                 <div>
                     <p>Package Name</p>
-                    <h2>General</h2>
-                        <br />
+                    <h2 className='text-xl text-black font-bold'>General</h2>
+                    <br />
                     <p>Amount</p>
-                    <h2>150 CHF</h2>
+                    <h2 className='text-xl text-black font-bold'>150 CHF</h2>
                 </div>
                 <div>
                     <p>Transaction Date</p>
                     <h2>2 March,2025</h2>
-                        <br />
+                    <br />
                     <p>Transaction Time</p>
                     <h2>11.00Am</h2>
                 </div>
+
+            </div>
+            <div className="flex border mt-10 border-[#1EB9C6] justify-between p-10 ">
+
+                <div className="flex">
+                    <div className=''>
+                        <img src={user} className='w-40 h-40 rounded-full'></img>
+                    </div>
+                    <div className=' ml-2'>
+                        <span className='text-2xl font-bold'>TO:</span>
+                        <p className='text-md'>User Name</p>
+                        <h2 className='text-2xl font-bold'>mr.provider</h2>
+                        <br />
+                        <p>Email</p>
+                        <p className='text-black'>ami@gmail.com</p>
+                    </div>
+                </div>
+                <div>
+                    <p>Amount</p>
+                    <h2 className='text-xl text-black font-bold'>150 CHF</h2>
+                    <br />
+                    <p>Transcation ID</p>
+                    <h2 className='text-xl text-black font-bold'>121511052585</h2>
+                </div>
+                <div>
+                    <p >Role</p>
+                    <h1 className='text-xl text-black '>Coustomer</h1>
+                    <br />
+                    <p>Reason</p>
+                    <h1 className='text-xl text-black '>Subscription Purchase</h1>
+                </div>
+
 
             </div>
         </div>
